4e3c5271acc4463c776f9f513caac2b87ac16b69
CURTIS <CSTARKEY5@GMAIL.com>
2022-02-03 22:45:11 +0800

    feb 3

---
+++ src/locations/rainbow-bridge-songshan.md
@@ -0,0 +1,17 @@
+---
+title: Rainbow Bridge Songshan
+city: Taipei
+slug: rainbow-bridge-songshan
+---
+
+# Rainbow Bridge (彩虹橋)
+
+One of the great things about living in Taipei is its hundreds of kilometers of riverside parks, which offer something for people of all ages. In addition, from select spots, they provide fantastic viewing for riverside sunsets. 
+
+Up in Songshan, conveniently right next to the classic Raohe Night Market, is the Rainbow Bridge, situated nicely along a beautiful riverside park. At all times of the day, but especially in the evening, this place gets filled with people. You can view the sunset from multiple places. Some choose to utilize the many benches next to the water. You can just snap shots from the bike/walking path on either side of the river. Depending on the sky, the sun might be obstructed by other bridges and buildings in the foreground. No worries; you can also take pictures from the bridge itself for a higher angle.
+
+The cool thing about this area is that it sits at a part of the Keelung River that turns northwest, so you have a chance to see the sunset and its rays shining across that part of the river. 
+
+The paths filled with pedestrians on either side of the river, the curves, the shadows, and at dusk, rainbow colors from the bridge and from street lamps make pictures from here jam-packed with detail and perspective. 
+
+Rainbow Bridge is easily accessible via public transport. The bridge is at the entrance to the entrance to the riverside park just a few minutes walk from Songshan Station. There is limited street parking in that area.
---
+++ src/pages/locations/little_nangang.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const little_nangang = () => {
+    
+    return (
+        <Layout>
+            <div>
+                <h1>Little Nangang Mountain (小南港山)</h1>
+                <p>This one’s fantastic if you’re looking for something simple and easily accessible by transit.</p>
+<p>Just a 7-minute walk from Nangang Exhibition Center Station will get you to the start of the Little Nangang Mountain trail. Hike 10-15 minutes up a series of stairs through a small forest, and you’ll reach the top of the mountain.</p>
+<p>On the way up, you’ll pass by a temple (終南洞三聖宮). To the side and behind the temple are a couple of places to view surrounding scenery as well as catch the sunset, although the view is obstructed by trees. Stay here if you like, or keep walking a few minutes to the top of the main staircase.</p>
+<p>Once you arrive at the peak, there is one main sunset-viewing platform, and really only one good angle on top of the mountain to see the sunset. So if there are other people, you might have to compete for picture-taking space. When I’ve gone during the week, there’s only been one or two other people up there around sunset time.</p>
+<p>The view of Nangang and Xinyi in the distance is pretty good, although the view from the main platform is obstructed in the middle by a tree, making it impossible to see the full skyline unless you have a drone.</p>
+<p>You can also go to a different viewpoint adjacent from the main platform that gives a great view of Xizhi ahead and Neihu/Songshan to the west. Depending on the season, you might also be able to catch a glimpse of the sun from here as it nears the horizon.</p>
+<p>This could be considered a more laid-back alternative to its bigger brother in the distance, Nangang Mountain, which is connected to the Four Beasts Mountain series of trails (these include the classic Elephant Mountain). Little Nangang Mountain is much easier to get to by MRT than Nangang Mountain, which requires a long walk from near Houshanpi Station on the Blue Line or Xiangshan Station on the Red Line.</p>
+            </div>
+        </Layout>
+    );
+}
+
+export default little_nangang
